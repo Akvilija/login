d364b1ce4c878098b19ba66c9579b0d7a1fc1d1d
Akvilija <akvilija.skorupskaite199503@gmail.com>
2025-02-18 18:10:40 +0200

    profile added

--- login-back-end/index.js
+++ login-back-end/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 
 const { connectToDB } = require('./db')
 const authRoutes = require('./routes/authRoutes')
+const profileRoutes = require('./routes/profileRoutes')
 
 const app = express()
 
@@ -12,6 +13,7 @@ app.use(express.json())
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/api/auth', authRoutes)
+app.use('/api/profile', profileRoutes)
 
 const port = process.env.PORT || 3000
 
